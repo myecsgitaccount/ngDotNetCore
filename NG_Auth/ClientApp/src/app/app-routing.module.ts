8a9a691cc3e64dfdcd709a721bc4b891b2996782
onyekachila <onyekachila@gmail.com>
2019-12-17 11:33:16 +0100

    login view design

--- NG_Auth/ClientApp/src/app/app-routing.module.ts
+++ NG_Auth/ClientApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [];
 
@@ -9,6 +10,7 @@ const routes: Routes = [];
 
     { path: "home", component: HomeComponent },
     { path: "", component: HomeComponent, pathMatch: "full" },
+    { path: "login", component: LoginComponent }, 
     { path: '**', redirectTo: '/home' }
 
   ])],
